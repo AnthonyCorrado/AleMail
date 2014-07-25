bd16476bc2c1f014de7ef65732f94774565c21f1
Anthony Corrado <amcstudios@yahoo.com>
2014-07-24 20:50:15 -0700

    seeded db for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(bar_name: "MonkeyMoos", username: "Princess")
