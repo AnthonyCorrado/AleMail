0e9e7584268d0ef7da9d582979c632c8852b5b28
Anthony Corrado <amcstudios@yahoo.com>
2014-07-25 19:37:03 -0700

    basic styling beer_list

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,5 +6,8 @@ class Beer < ActiveRecord::Base
 	before_save { self.name = name.titleize }
 	before_save	{ self.brewery = brewery.titleize }
 	before_save { self.style = style.titleize }
+	before_save { if self.style == "Ipa"
+									self.style = self.style.upcase 
+								end }
 	before_save	{ self.brewery_location = brewery_location.titleize }
 end
