2f3a80cfb6230799332eb0964084fb392b811556
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-14 20:57:12 -0700

    jquery playing nice with inputs

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,14 @@
 jQuery(document).ready(function($){
+	// hidden phone/email fields become visible in edit page if filled
+	if( $('input#patron_phone').val()){
+		$(".hidden-phone-field").toggle();
+		$('input[name="phoneCheckbox"]').each(function(){ this.checked = true; });
+	}
+	if( $('input#patron_email').val()){
+		$(".hidden-email-field").toggle();
+		$('input[name="emailCheckbox"]').each(function(){ this.checked = true; });
+	}
+	// sets checkbox action to hide/show phone and email fields
 	$('input[type="checkbox"]').click(function(){
     if($(this).attr("value")=="phone"){
         $(".hidden-phone-field").toggle();
@@ -7,6 +17,7 @@ jQuery(document).ready(function($){
         $(".hidden-email-field").toggle();
     }
 	});
+	// if first beer is selected, second beer selection becomes available, ect. 
 	$('#beer_choice_one').change(function(){
 		if($('#beer_choice_one').val() !== null){
 			$(".beer-select-two").css("display", "block");
