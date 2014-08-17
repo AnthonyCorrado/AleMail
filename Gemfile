ec8fda5a64461441b7b6951cf95a4b9bffb88319
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-17 02:39:38 -0700

    adding text service

--- Gemfile
+++ Gemfile
@@ -29,7 +29,6 @@ gem 'spring',        group: :development
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
 # Use unicorn as the app server
 # gem 'unicorn'
 
@@ -40,3 +39,4 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'debugger', group: [:development, :test]
 gem 'twilio-ruby'
 gem 'rails_12factor'
+gem 'figaro'
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+
   def index
   	@user = User.find(current_user)
   	@notifications = @user.notifications
@@ -12,13 +13,14 @@ class NotificationsController < ApplicationController
     @notification = Notification.new
   	@user = User.find(current_user)
     @beer = @user.beers
-
   end
 
   def create
-    @patron = Patron.last
-    text_updates(@patron)
   	@notification = Notification.new(notification_params)
+    @patrons = Patron.all
+    @patrons = @patrons.where(beer_id: @notification.beer_id)
+    text_updates(@patrons, @notification.message_content)
+    # text_updates(@notification)
   	if @notification.save
   		redirect_to user_path(current_user)
   	else
@@ -26,13 +28,25 @@ class NotificationsController < ApplicationController
   	end
   end
 
-  def text_updates(patron)
+  def text_updates(patrons, message_content)
+    @client = Twilio::REST::Client.new ENV['TWILIO_ID'], ENV['TWILIO_SECRET']
+    patrons.each do |p|
+      if p.phone != "" 
+        @client.account.messages.create({
+          :from => 
+          :to => 
+          :body => "Message: #{message_content}"
+          })
+        puts p.phone
+      end
+    end
+    puts message_content
+    puts "chicken yoshi matt"
   end
 
   private
 
   def notification_params
   	params.require(:notification).permit(:user_id, :beer_id, :patron_id, :message_content)
-  end
-  
+  end 
 end
